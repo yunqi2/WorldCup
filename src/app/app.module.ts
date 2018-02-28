7a733877c2028d1d0e8777bcad5333e0673dfe62
黄小辉 <hdh2013@126.com>
2018-02-28 15:23:19 +0800

    搭建框架 首页、合买、开奖、我的

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { BuytogetherPage } from '../pages/buytogether/buytogether';
+import { LotteryPage } from '../pages/lottery/lottery';
+import { MinePage } from '../pages/mine/mine';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    BuytogetherPage,
+    LotteryPage,
+    MinePage,
     HomePage,
     TabsPage
   ],
@@ -26,8 +28,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    BuytogetherPage,
+    LotteryPage,
+    MinePage,
     HomePage,
     TabsPage
   ],
