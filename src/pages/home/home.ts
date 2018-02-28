235c6437cc17590178fba5013decbda1cdc199e7
黄小辉 <hdh2013@126.com>
2018-02-28 18:36:18 +0800

    .

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Slides } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild(Slides) slides: Slides;
   constructor(public navCtrl: NavController) {
 
   }
-
+  /**
+   * 用户滑动后 继续自动滑动
+   */
+  continueToSlide(){
+   this.slides.startAutoplay();
+  }
+  goto(){
+    var i=this.slides.getActiveIndex();
+    console.log(i);
+  }
+  log(){
+    console.log("hello k");
+    
+  }
 }
